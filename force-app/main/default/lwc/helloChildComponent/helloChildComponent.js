376698a2f5710d1f15904e36040ea090d77a646c
Greg Wax <gwax@deltadentalmn.org>
2025-03-31 08:57:51 -0500

    LWC-2025-03-31 HelloQuerySelectorDemo

--- force-app/main/default/lwc/helloChildComponent/helloChildComponent.js
+++ force-app/main/default/lwc/helloChildComponent/helloChildComponent.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloChildComponent extends LightningElement {}
+export default class HelloChildComponent extends LightningElement {
+    name = "Greg Wax";
+}
